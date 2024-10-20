31b6217a48c67057b5f6a7774471b1b0306d3038
LOMAGOSH <123672145+LOMAGOSH@users.noreply.github.com>
2024-10-20 14:34:11 -0500

    Testing_Password

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,3 +1,5 @@
+def test_password(x):
+    print(x)
 #This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
@@ -10,3 +12,11 @@ print(f"x = {x} and y = {y}")
 
 #We could also use an f-string like this:
 print("c = {c} and d = {d}".format(c=1, d=2))
+
+job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
+
+for name, job in job_list.items():
+    print(f"{name} is a {job}")
+
+Password = "Student123!"
+test_password(Password)
